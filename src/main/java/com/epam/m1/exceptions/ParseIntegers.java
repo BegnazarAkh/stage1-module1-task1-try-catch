2bb4781129bee05ad1cf620c1f9f034198060de9
BegnazarAkh <begnazaraxmadjonov@mail.ru>
2023-08-27 12:37:41 +0200

    Stage 1 Task1 Try-Catch

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -12,7 +12,7 @@ public class ParseIntegers {
 
     private static final List<String> WORDS = Arrays.asList(
             "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
-                    .split("\\s+")); // Use "\\s+" to split by spaces
+                    .split("\n+")); // Use "\\s+" to split by spaces
 
 
     public static void main(String[] args) {
