0bb185cf32d4dcde6996aad839aa6a014ad04f39
BegnazarAkh <begnazaraxmadjonov@mail.ru>
2023-09-07 20:29:19 +0200

    Stage 1 Task1 Try-Catch

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -1,8 +1,11 @@
 package com.epam.m1.exceptions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.lang.*;
+import java.util.stream.Collectors;
 
 /**
  * Complete the code, parse integers, calculate the sum of numbers in the WORDS, join strings with
@@ -12,8 +15,7 @@ public class ParseIntegers {
 
     private static final List<String> WORDS = Arrays.asList(
             "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
-                    .split("\n+")); // Use "\\s+" to split by spaces
-
+                    .split(" "));
 
     public static void main(String[] args) {
         Iterator<String> words = WORDS.iterator();
@@ -23,13 +25,22 @@ public class ParseIntegers {
             String next = words.next();
             try {
                 int number = Integer.parseInt(next);
-                sum += number;
             } catch (NumberFormatException e) {
-                justWords += next + " ";
+                e.getMessage();
             }
         }
+
+        for (String word : WORDS) {
+            if (word.matches("\\d+")) {
+                sum += Integer.parseInt(word);
+            }
+        }
+
+        String forJustWords = " JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations.";
+
+        justWords = forJustWords.replaceAll("\\d+ ", "");
+
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
     }
 }
-
